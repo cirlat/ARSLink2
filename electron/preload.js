02bb4a8d638cf8da848e06c1f39310087fa739ec
cirlat <ciro.latela@gmail.com>
2025-03-12 23:10:58 +0100

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -1,4 +1,4 @@
-const { contextBridge, ipcRenderer } = require("electron");
+import { contextBridge, ipcRenderer } from "electron";
 
 // Esponi le API sicure alla finestra del renderer
 contextBridge.exposeInMainWorld("electronAPI", {
